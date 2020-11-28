89e00d3e5251c291a338f3324deca3a9bb3c73c3
Mykola Gladchenko <mykola.gladchenko@gmail.com>
2020-11-28 18:59:53 +0200

    Added missing files from prev commit

---
+++ src/test/java/step/RunJBehaveScenarios.java
@@ -0,0 +1,8 @@
+package step;
+
+import net.serenitybdd.jbehave.SerenityStories;
+import net.serenitybdd.jbehave.annotations.Metafilter;
+
+@Metafilter("+math-operations")
+public class RunJBehaveScenarios extends SerenityStories {
+}
